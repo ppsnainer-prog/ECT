7dc3ff8a4ff6f4ddf353f1e5ed5abda171707f71
ppsnainer-prog <ppsnainer@gmail.com>
2026-09-13 00:27:21 +1000

    Add files via upload

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
  * После изменения: сохранить -> git push -> дождаться GitHub Pages.
  */
 window.ECT_CONFIG = Object.freeze({
-  version: '2.7.26',
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxrm-NNr1atagqhccEI6DGd-x9qejciVhFCsXHYQ8MNBtdGQV2rgwLIMk8kV4w0Fu3gFQ/exec'
+  version: '2.7.27',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxpRTJReqDRX2qKT_MLJpHYX89SpBy2cxnYxuG7uB3aFJL-I_Qs9O5Qw6101yf-K6w5/exec'
 });
